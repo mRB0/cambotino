f490b74a379f033ffd027db21cd6542def63464f
Mike Burke <mrburke@gmail.com>
2013-09-01 18:24:57 -0300

    Fix const char pointers

--- printf.h
+++ printf.h
@@ -110,10 +110,10 @@ regs Kusti, 23.10.2004
 
 void init_printf(void* putp,void (*putf) (void*,char));
 
-void tfp_printf(char *fmt, ...);
-void tfp_sprintf(char* s,char *fmt, ...);
+void tfp_printf(char const* fmt, ...);
+void tfp_sprintf(char * s, char const *cfmt, ...);
 
-void tfp_format(void* putp,void (*putf) (void*,char),char *fmt, va_list va);
+void tfp_format(void* putp,void (*putf) (void*,char),char const *fmt, va_list va);
 
 #define printf tfp_printf 
 #define sprintf tfp_sprintf 
